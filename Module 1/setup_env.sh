e295a1708f7264169ba0e3389a6fb51c5461beb8
matteopilotto <matteo.pilotto@protonmail.com>
2023-07-21 20:27:54 +0200

    update setup_env.sh

--- Module 1/setup_env.sh
+++ Module 1/setup_env.sh
@@ -1,7 +1,11 @@
-
-pip install -U spacy
-pip install -U gensim
-pip install -U pandas
-pip install -U matplotlib
+pip install spacy==3.6.0
+pip install gensim==4.3.1
+pip install pandas==2.0.3
+pip install matplotlib==3.7.2
+pip install numpy==1.25.1
+pip install pandas==2.0.3
+pip install pandasql==0.7.3
+pip install scikit-learn==1.3.0
+pip install tqdm==4.65.0
 python -m spacy download en_core_web_sm
 python -m spacy download en_core_web_md
